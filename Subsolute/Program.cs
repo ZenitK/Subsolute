a0c04fa653e320e3614e0f6cbba22bbff30c3538
Zenit Kovačević <zenit.kovacevic@gmail.com>
2021-01-14 14:32:33 +0100

    Initial commit

---
+++ Subsolute/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Subsolute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
