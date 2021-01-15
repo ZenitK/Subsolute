1231a6e48914151d7a5622fb17d17bb8352dd8e1
Zenit Kovačević <zenit.kovacevic@gmail.com>
2021-01-15 20:10:23 +0100

    reordering of some methods

--- Subsolute/SolutionBuilder.cs
+++ Subsolute/SolutionBuilder.cs
@@ -13,59 +13,6 @@ namespace Subsolute
     {
         private readonly XmlSerializer _xmlSerializer = new(typeof(Project));
 
-        private static string ExtractProjectGuid(Project deserializedProject, string projectPath)
-        {
-            var guids = deserializedProject
-                .PropertyGroup
-                .Select(x => x.ProjectGuid)
-                .Where(x => !string.IsNullOrWhiteSpace(x))
-                .ToList();
-
-            if (guids.Count > 1)
-            {
-                throw new Exception($"The project {projectPath} seems to have multiple Guids " +
-                                    $"[{string.Join(",", guids)}]");
-            }
-
-            return guids.SingleOrDefault();
-        }
-
-        private List<Node> ExtractChildren(Project deserializedProject, string parentFullPath) =>
-            deserializedProject
-                .ItemGroup
-                .SelectMany(x => x.ProjectReference)
-                .Select(x =>
-                {
-                    var fullPath = FindChildFullPath(parentFullPath, x.Include);
-                    return BuildProjectTree(fullPath);
-                })
-                .ToList();
-
-        private static string FindChildFullPath(string parentFullPath, string childPath)
-        {
-            var includedProject = childPath.Replace('\\', '/');
-            var parentDirectory = GetDirectoryName(parentFullPath);
-            var projectPath = Combine(parentDirectory ?? throw new InvalidOperationException(), includedProject);
-            var fullPath = GetFullPath(projectPath);
-            return fullPath;
-        }
-
-        private Project DeserializeProject(string projectPath)
-        {
-            using var fileReader = new FileStream(projectPath, FileMode.Open);
-
-            var deserializedProject = _xmlSerializer.Deserialize(fileReader) as Project;
-            return deserializedProject;
-        }
-
-        private static void CheckIfFileExists(string projectPath)
-        {
-            if (!File.Exists(projectPath))
-            {
-                throw new FileNotFoundException($"csproj file {projectPath} not found");
-            }
-        }
-
         public Node BuildProjectTree(string projectPath)
         {
             CheckIfFileExists(projectPath);
@@ -83,6 +30,59 @@ namespace Subsolute
                 ProjectGuid = projectGuid,
                 Children = children
             };
+        }
+        
+        private List<Node> ExtractChildren(Project deserializedProject, string parentFullPath) =>
+            deserializedProject
+                .ItemGroup
+                .SelectMany(x => x.ProjectReference)
+                .Select(x =>
+                {
+                    var fullPath = FindChildFullPath(parentFullPath, x.Include);
+                    return BuildProjectTree(fullPath);
+                })
+                .ToList();
+
+        private static void CheckIfFileExists(string projectPath)
+        {
+            if (!File.Exists(projectPath))
+            {
+                throw new FileNotFoundException($"csproj file {projectPath} not found");
+            }
+        }
+
+        private static string ExtractProjectGuid(Project deserializedProject, string projectPath)
+        {
+            var guids = deserializedProject
+                .PropertyGroup
+                .Select(x => x.ProjectGuid)
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToList();
+
+            if (guids.Count > 1)
+            {
+                throw new Exception($"The project {projectPath} seems to have multiple Guids " +
+                                    $"[{string.Join(",", guids)}]");
+            }
+
+            return guids.SingleOrDefault();
+        }
+
+        private static string FindChildFullPath(string parentFullPath, string childPath)
+        {
+            var includedProject = childPath.Replace('\\', '/');
+            var parentDirectory = GetDirectoryName(parentFullPath);
+            var projectPath = Combine(parentDirectory ?? throw new InvalidOperationException(), includedProject);
+            var fullPath = GetFullPath(projectPath);
+            return fullPath;
+        }
+
+        private Project DeserializeProject(string projectPath)
+        {
+            using var fileReader = new FileStream(projectPath, FileMode.Open);
+
+            var deserializedProject = _xmlSerializer.Deserialize(fileReader) as Project;
+            return deserializedProject;
         }
     }
 }
