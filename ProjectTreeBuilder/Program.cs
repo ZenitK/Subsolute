35c548d647d288d6be59c7438998b57dd0f920b6
Zenit Kovačević <zenit.kovacevic@gmail.com>
2021-01-15 16:07:35 +0100

    added a utility to print small trees

---
+++ ProjectTreeBuilder/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AsciiTreeDiagram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the list of nodes
+            var topLevelNodes = CreateNodeList();
+            
+            var printer = new TreePrinter();
+            
+            foreach (var node in topLevelNodes)
+            { 
+                printer.PrintNode(node);
+            }
+
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.Read();
+            }
+        }
+
+        private static List<Node> CreateNodeList() =>
+            new()
+            {
+                new()
+                {
+                    Name = "Default",
+                    Children =
+                    {
+                        new Node
+                        {
+                            Name = "Package",
+                            Children = {
+                                new Node
+                                {
+                                    Name = "Zip-Files",
+                                    Children = {
+                                        new Node
+                                        {
+                                            Name = "Copy-Files",
+                                            Children = {
+                                                new Node
+                                                {
+                                                    Name = "Run-Unit-Tests",
+                                                    Children = {
+                                                        new Node
+                                                        {
+                                                            Name = "Build",
+                                                            Children = {
+                                                                new Node
+                                                                {
+                                                                    Name = "Restore-NuGet-Packages",
+                                                                    Children = {
+                                                                        new Node { Name = "Clean" }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+
+                                new Node
+                                {
+                                    Name = "Create-Nuget-Packages",
+                                    Children = {
+                                        new Node
+                                        {
+                                            Name = "Copy-Files",
+                                            Children = {
+                                                new Node
+                                                {
+                                                    Name = "Run-Unit-Tests",
+                                                    Children = {
+                                                        new Node
+                                                        {
+                                                            Name = "Build",
+                                                            Children = {
+                                                                new Node
+                                                                {
+                                                                    Name = "Restore-NuGet-Packages",
+                                                                    Children = {
+                                                                        new Node { Name = "Clean" }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                new()
+                {
+                    Name = "AppVeyor",
+                    Children =
+                    {
+                        new Node
+                        {
+                            Name = "Upload-AppVeyor-Artifacts",
+                            Children = {
+                                new Node
+                                {
+                                    Name = "Sign-Binaries",
+                                    Children = {
+                                        new Node
+                                        {
+                                            Name = "Zip-Files",
+                                            Children = {
+                                                new Node
+                                                {
+                                                    Name = "Run-Unit-Tests",
+                                                    Children = {
+                                                        new Node
+                                                        {
+                                                            Name = "Build",
+                                                            Children = {
+                                                                new Node
+                                                                {
+                                                                    Name = "Restore-NuGet-Packages",
+                                                                    Children = {
+                                                                        new Node { Name = "Clean" }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+
+                                new Node
+                                {
+                                    Name = "Create-Nuget-Packages",
+                                    Children = {
+                                        new Node
+                                        {
+                                            Name = "Copy-Files",
+                                            Children = {
+                                                new Node
+                                                {
+                                                    Name = "Run-Unit-Tests",
+                                                    Children = {
+                                                        new Node
+                                                        {
+                                                            Name = "Build",
+                                                            Children = {
+                                                                new Node
+                                                                {
+                                                                    Name = "Restore-NuGet-Packages",
+                                                                    Children = {
+                                                                        new Node { Name = "Clean" }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+    }
+}
---
+++ ProjectTreeBuilder/TreePrinter.cs
@@ -0,0 +1,55 @@
+using System;
+
+/*
+ * Code shamelessly taken with small adjustments from
+ * https://github.com/andrewlock/blog-examples/tree/master/AsciiTreeDiagram/AsciiTreeDiagram.
+ * Described in Andrew Lock's blog post https://andrewlock.net/creating-an-ascii-art-tree-in-csharp/
+ */
+namespace AsciiTreeDiagram
+{
+    public class TreePrinter
+    {
+        // Constants for drawing lines and spaces
+        private const string Cross = " ├─";
+        private const string Corner = " └─";
+        private const string Vertical = " │ ";
+        private const string Space = "   ";
+        
+        internal void PrintNode(Node node, string indent = "")
+        {
+            Console.WriteLine(node.Name);
+
+            // Loop through the children recursively, passing in the
+            // indent, and the isLast parameter
+            var childrenCount = node.Children.Count;
+            for (var i = 0; i < childrenCount; i++)
+            {
+                var child = node.Children[i];
+                var isLast = i == (childrenCount - 1);
+                PrintChildNode(child, indent, isLast);
+            }
+        }
+
+        private void PrintChildNode(Node node, string indent, bool isLast)
+        {
+            // Print the provided pipes/spaces indent
+            Console.Write(indent);
+
+            // Depending if this node is a last child, print the
+            // corner or cross, and calculate the indent that will
+            // be passed to its children
+            if (isLast)
+            {
+                Console.Write(Corner);
+                indent += Space;
+            }
+            else
+            {
+                Console.Write(Cross);
+                indent += Vertical;
+            }
+
+            PrintNode(node, indent);
+        }
+    }
+}
